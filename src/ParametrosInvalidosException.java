fcbe511409b480d9a90ae48534aa4c2b3a352887
Antonio Tiburtino <antonio_ammt@hotmail.com>
2024-12-16 20:59:02 -0300

    Adicionando o projeto controle de fluxo

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(){
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro parâmetro.");
+    }
+}
